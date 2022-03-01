ae020644a259d8628bc7ec6ed299d6c171431e7e
filza zehra <filza12@hotmail.com>
2022-02-28 20:19:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,43 +55,29 @@ def main():
       bytes_data = uploaded_file.read()
       st.write("filename:", uploaded_file.name)
       st.write(bytes_data)
-      
-      uploaded_file = st.file_uploader("Choose a image file - file format must be JPEG,JPG PR PNG ONLY", type = ['jpeg','jpg','png'])
-      
-      if uploaded_file is not None:
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-        opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-        resized = cv2.resize(opencv_image,(256,256))
-        st.image(opencv_image, channels="RGB")
-        save_uploadedfile(uploaded_file)
-        resized = mobilenet_v2_preprocess_input(resized)
-        img_reshape = resized[np.newaxis,...]
-        
-    with open(uploaded_file.name,"wb") as f:
-      f.write(uploaded_file.getbuffer())
-    
+       
   elif choice == "About":
     st.write("about app")
 if __name__ == '__main__':
         main()
     
-#uploaded_file = st.file_uploader("Choose a image file - file format must be JPEG,JPG PR PNG ONLY", type = ['jpeg','jpg','png'])
+uploaded_file = st.file_uploader("Choose a image file - file format must be JPEG,JPG PR PNG ONLY", type = ['jpeg','jpg','png'])
 
-#if uploaded_file is not None:
+if uploaded_file is not None:
   # Convert the file to an opencv image.
-  #file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-  #opencv_image = cv2.imdecode(file_bytes, 1)
-  #opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-  #resized = cv2.resize(opencv_image,(256,256))
+  file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+  opencv_image = cv2.imdecode(file_bytes, 1)
+  opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+  resized = cv2.resize(opencv_image,(256,256))
   # Now do something with the image! For example, let's display it:
-  #st.image(opencv_image, channels="RGB")
-  #save_uploadedfile(uploaded_file)
-  #resized = mobilenet_v2_preprocess_input(resized)
-  #img_reshape = resized[np.newaxis,...]
+  st.image(opencv_image, channels="RGB")
+  save_uploadedfile(uploaded_file)
+  resized = mobilenet_v2_preprocess_input(resized)
+  img_reshape = resized[np.newaxis,...]
   
-  #with open(uploaded_file.name,"wb") as f:
-    #f.write(uploaded_file.getbuffer())
+  with open(uploaded_file.name,"wb") as f:
+    f.write(uploaded_file.getbuffer())
+    predict = st.button("Predict")
     
       
 
@@ -157,7 +143,7 @@ class Meso4(Classifier):
     y = Dense(1, activation = 'sigmoid')(y)
 
     return Model(inputs = x, outputs = y)
-if st.button("Predict"):
+if Predict:
       meso = Meso4()
       meso.load("saved_model/Meso4_DF")
                                       #image generator to predict images

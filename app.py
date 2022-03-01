4049e0a17bcbdccd7968ae03fba8402053e7f375
filza zehra <filza12@hotmail.com>
2022-02-28 20:23:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def main():
       st.write(bytes_data)
        
   elif choice == "About":
-    st.write("This App let's you Predict Deep Fake Images")
+    st.write("This  app let's you Predict Deep Fake Images")
 if __name__ == '__main__':
         main()
     

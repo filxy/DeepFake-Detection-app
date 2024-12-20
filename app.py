629d5316166d4ac627833e13692871095ee20e1f
filza zehra <filza12@hotmail.com>
2024-12-19 23:45:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,23 +12,13 @@ import PIL
 import io
 import html
 import time
-import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D
-from tensorflow.keras.layers import BatchNormalization, Dropout, Reshape, Concatenate
-from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Reshape, Concatenate, LeakyReLU
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
-from tensorflow.keras.applications.mobilenet_v3 import MobileNetV3Small, preprocess_input as mobilenet_v3_preprocess_input
-#from tensorflow import keras
-#from tensorflow.keras import layers
-#from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Reshape, Concatenate, LeakyReLU
-#from tensorflow.keras.preprocessing.image import ImageDataGenerator
-#from tensorflow.keras.optimizers import Adam
-#from tensorflow.keras.models import Model
-#from tensorflow.keras.applications.mobilenet_v3 import MobileNetV3,preprocess_input as mobilenet_v3_preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 from scipy.ndimage.interpolation import zoom,rotate
 import random
 import os
@@ -72,7 +62,7 @@ def main():
       # Now do something with the image! For example, let's display it:
       st.image(opencv_image, channels="RGB")
       save_uploadedfile(uploaded_file)
-      resized = mobilenet_v3_preprocess_input(resized)
+      resized = mobilenet_v2_preprocess_input(resized)
       img_reshape = resized[np.newaxis,...]
       with open(uploaded_file.name,"wb") as f:
         f.write(uploaded_file.getbuffer())
